6e6081b601ec6d9df176cfde0ef6eeba1b25cf4d
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2022-02-06 16:23:19 +0200

    removed flash effect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import AppBar from "./components/AppBar/AppBar";
 import ContactsView from "./views/ContactsView";
@@ -7,48 +7,51 @@ import HomeView from "./views/HomeView";
 import RegisterView from "./views/RegisterView";
 import LoginView from "./views/LoginView";
 import Container from "./components/Container";
-import { authOperations } from "./redux/auth";
+import { authOperations, authSelectors } from "./redux/auth";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import PublicRoute from "./components/PublicRoute/PublicRoute";
 
 const App = () => {
   const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
 
   useEffect(() => {
     dispatch(authOperations.fetchCurrentUser());
   }, [dispatch]);
 
   return (
-    <Container>
-      <AppBar />
-      <Routes>
-        <Route path="/" element={<HomeView />} />
-        <Route
-          path="/register"
-          element={
-            <PublicRoute restricted redirectTo="/contacts">
-              <RegisterView />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <PublicRoute restricted redirectTo="/contacts">
-              <LoginView />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/contacts"
-          element={
-            <PrivateRoute>
-              <ContactsView />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </Container>
+    !isFetchingCurrentUser && (
+      <Container>
+        <AppBar />
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute restricted redirectTo="/contacts">
+                <RegisterView />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute restricted redirectTo="/contacts">
+                <LoginView />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute>
+                <ContactsView />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Container>
+    )
   );
 };
 
